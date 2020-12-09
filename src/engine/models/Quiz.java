272642a19b0b965133af5bb172aeed1d19ef8432
karolh95 <karolh95@live.com>
2020-12-09 10:01:55 +0100

    Added fields: id, answer to the Quiz class

--- src/engine/models/Quiz.java
+++ src/engine/models/Quiz.java
@@ -1,12 +1,20 @@
 package engine.models;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Quiz {
+
     private final String title;
     private final String text;
     private final String[] options;
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private final int answer;
+    @Setter
+    private int id;
 }
