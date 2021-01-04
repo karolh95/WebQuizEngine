50792b0774afc3c50d1b43dc8e7bb0cb0fa49bcb
karolh95 <karolh95@live.com>
2021-01-04 15:30:42 +0100

    Allowed methods and origin added to Cors configuration.

--- src/engine/configs/SecurityConfig.java
+++ src/engine/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package engine.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,10 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value(("${ALLOWED_ORIGIN}"))
+    private String ALLOWED_ORIGIN;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,6 +33,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/actuator/shutdown").permitAll()
                 .anyRequest().authenticated()
         ;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.addAllowedMethod(HttpMethod.DELETE);
+        configuration.addAllowedMethod(HttpMethod.PUT);
+        configuration.setAllowedOrigins(List.of(ALLOWED_ORIGIN));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
